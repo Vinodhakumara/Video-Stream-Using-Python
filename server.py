8ca89b2814ea3e8be94cb5a4829cae71843e53da
vinodha kumara L <vinodhakumara2681997@gmail.com>
2021-08-12 20:02:06 +0530

    Second commit

--- server.py
+++ server.py
@@ -12,11 +12,14 @@ Requirements
 """
 import cv2, socket, numpy, pickle    # Import Modules
 
+# AF_INET refers to the address of family of ip4v
+# SOCK_DGRAM means connection oriented UDP protocol
 s=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)  # Gives UDP protocol to follow
 ip="192.168.43.149"   # Server public IP
-port=307              # Server port number
+port=307              # Server Port Number to identify the process that needs to recieve or send packets
 s.bind((ip,port))     # Bind the IP:port to connect 
 
+# In order to iterate over block of code as long as test expression is true
 while True:
     x=s.recvfrom(100000000)    # Recieve byte code sent by client using recvfrom
     clientip = x[1][0]         # x[1][0] in this client details stored,x[0][0] Client message Stored
